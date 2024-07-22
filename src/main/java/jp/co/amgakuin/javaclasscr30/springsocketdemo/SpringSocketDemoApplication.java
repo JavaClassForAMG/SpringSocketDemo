df9391cc0f968fc09990c50fb3d57612163fa768
Nobuyuki Kimura <a09@r-force.co.jp>
2024-07-22 10:05:13 +0900

    First Commit

---
+++ src/main/java/jp/co/amgakuin/javaclasscr30/springsocketdemo/SpringSocketDemoApplication.java
@@ -0,0 +1,15 @@
+package jp.co.amgakuin.javaclasscr30.springsocketdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSocketDemoApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SpringSocketDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jp/co/amgakuin/javaclasscr30/springsocketdemo/SpringSocketDemoApplicationTests.java
@@ -0,0 +1,15 @@
+package jp.co.amgakuin.javaclasscr30.springsocketdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSocketDemoApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
